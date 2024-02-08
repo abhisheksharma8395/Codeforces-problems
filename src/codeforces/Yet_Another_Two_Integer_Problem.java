189e1469666e60ff05fb55e3db35f5813126786e
abhisheksharma8395 <122424312+abhisheksharma8395@users.noreply.github.com>
2024-02-08 16:47:15 +0530

    main.java

--- src/codeforces/Yet_Another_Two_Integer_Problem.java
+++ src/codeforces/Yet_Another_Two_Integer_Problem.java
@@ -8,29 +8,29 @@ public class Yet_Another_Two_Integer_Problem {
         Scanner sc=new Scanner(System.in);
         int n,i;
         n=sc.nextInt();
-        int myarr[]=new int[n];
+        int[] myarr1=new int[n];
         for (i = 0; i <n ; i++) {
             int a=sc.nextInt();
             int b=sc.nextInt();
             if(a>b){
                 if(((a-b)%10)==0){
-                    myarr[i]=((a-b)/10);
+                    myarr1[i]=((a-b)/10);
                 }
                 else{
-                    myarr[i]=(((a-b)/10)+1);
+                    myarr1[i]=(((a-b)/10)+1);
                 }
             }
             else{
                 if(((b-a)%10)==0){
-                    myarr[i]=((b-a)/10);
+                    myarr1[i]=((b-a)/10);
                 }
                 else{
-                    myarr[i]=(((b-a)/10)+1);
+                    myarr1[i]=(((b-a)/10)+1);
                 }
             }
         }
         for (i = 0; i <n; i++) {
-            System.out.println(myarr[i]+" ");
+            System.out.println(myarr1[i]+" ");
         }
     }
 
