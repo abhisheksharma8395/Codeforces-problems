e5c7f5886c61c219febd19dc5284d67658e18724
abhisheksharma8395 <122424312+abhisheksharma8395@users.noreply.github.com>
2024-02-14 17:57:03 +0530

    solved Codeforces problem Beautiful_Year_271_A.java

--- src/codeforces/Beautiful_Year_271_A.java
+++ src/codeforces/Beautiful_Year_271_A.java
@@ -1,5 +1,23 @@
 package codeforces;
 
-public class Beautiful_Year_271_A {
+import java.util.Scanner;
 
+public class Beautiful_Year_271_A {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n;
+        n = sc.nextInt();
+        n = n + 1;
+        while (true) {
+            int o = n % 10;
+            int t = (n % 100) / 10;
+            int h = (n % 1000) / 100;
+            int th = (n / 1000);
+            if (o != t && o != h && o != th && t != h && t != th && h != th) {
+                System.out.println(n);
+                break;
+            }
+            n++;
+        }
+    }
 }
